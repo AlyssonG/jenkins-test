5dfdf767562caa3bb22811780876d5e063f462ad
Alysson Gomes <alysson.gomes@bemobi.com.br>
2017-07-24 17:23:58 -0300

    Adicionado novo teste

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,4 +8,9 @@ public class AppTest {
     public void testPrintHelloWorld(){
         Assertions.assertEquals(App.getHelloWorld(), "Hello World","A mensagem é: " + App.getHelloWorld());
     }
+
+    @Test
+    public void testPrintHelloWorld2(){
+        Assertions.assertEquals(App.getHelloWorld2(), "Hello World","A mensagem é: " + App.getHelloWorld2());
+    }
 }

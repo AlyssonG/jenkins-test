a156201cf53228a5a6e22b71729dbac0f1db137e
Alysson Gomes <alysson.gomes@bemobi.com.br>
2017-07-24 17:18:55 -0300

    Mudança no teste

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class AppTest {
 
+    @Test
     public void testPrintHelloWorld(){
         Assertions.assertEquals(App.getHelloWorld(), "Hello World","A mensagem é: " + App.getHelloWorld());
     }
